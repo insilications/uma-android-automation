9beef05d052bf21a3b56fa74777d7a787f83dcfa
Steve Tu <stevetu11@yahoo.com>
2025-08-09 10:06:28 -0700

    Apply minor formatting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,10 @@
 include(":app")
 rootProject.name = "UmaAndroidAutomation"
 
-//Automatic provisioning of a compatible JVM toolchain.
-//Convention plugin fetches a jdk into the gradle home directory
-//if it doesn't find any compatible ones in its canonical
-//OS search paths.
+// Automatic provisioning of a compatible JVM toolchain.
+// Convention plugin fetches a jdk into the gradle home directory
+// if it doesn't find any compatible ones in its canonical OS search paths.
 plugins {
-    //Settings plugins cannot be declared in version catalog
+    // Settings plugins cannot be declared in version catalog.
     id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
